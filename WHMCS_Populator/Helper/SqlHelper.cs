72c210184bb7260847a60190f67aa479014aa9f2
milivoj-kovacevic-devtech <milivoj.kovacevic@devtechgroup.com>
2015-02-06 00:25:08 +0100

    added helper class for mysql db connections

---
+++ WHMCS_Populator/Helper/SqlHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WHMCS_Populator.Helper
+{
+    public class SqlHelper
+    {
+
+        public static MySqlConnection WhmcsConnection(string connectionString)
+        {
+            // TODO Should change this to more dinamic solution
+            return new MySqlConnection(connectionString);
+        }
+
+        public static string SelectClientsIds()
+        {
+            var queryBuilder = new SelectQueryBuilder();
+            string client = String.Empty; // TODO Change this to const with table name
+
+            queryBuilder.SelectFromTable(client);
+            queryBuilder.SelectColumns(client + ".id", client + ".parent_id"); // TODO Change this to match whmcs db
+        }
+    }
+}
