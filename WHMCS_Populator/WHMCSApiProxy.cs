3655ebb96105e78566111b5cf1b5cb889e523c56
milivoj-kovacevic-devtech <milivoj.kovacevic@devtechgroup.com>
2015-02-05 23:40:37 +0100

    started with some ideas for populator

---
+++ WHMCS_Populator/WHMCSApiProxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHMCS_Populator
+{
+    class WHMCSApiProxy
+    {
+        internal struct WHMCSApi
+        {
+            public const string AddClient = "addclient";
+            public const string GetClients = "getclients";
+            public const string AddContact = "addcontact";
+
+            public const string ModuleCreate = "modulecreate";
+        }
+    }
+}
