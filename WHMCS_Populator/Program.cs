91b9840a1430352a0b23cc23c563a67134d76b26
milivoj-kovacevic-devtech <milivoj.kovacevic@devtechgroup.com>
2015-02-06 16:21:36 +0100

    added general idea for sending http requests

--- WHMCS_Populator/Program.cs
+++ WHMCS_Populator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,21 @@ namespace WHMCS_Populator
     {
         static void Main(string[] args)
         {
+			var userName = "zoran.prole";
+			var password = "cc03e747a6afbbcbf8be7668acfebee5";
 
+			WebRequest request = WebRequest.Create("http://178.248.110.214/whmcs/includes/api.php");
+			request.Credentials = new NetworkCredential(userName, password);
+
+			((HttpWebRequest)request).UserAgent = ".Net Framework Test";
+			request.Method = "POST";
+			request.ContentLength = 92;
+			request.ContentType = "application/x-www-form-urlencoded";
+
+			Stream dataStream = request.GetRequestStream();
+			// TODO Implement
+
+			WebResponse response = request.GetResponse();
         }
     }
 }
