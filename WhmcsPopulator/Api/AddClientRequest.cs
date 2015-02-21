1f1fe8f4d78ac2260c322694a60aa858306e60e7
milivoj-kovacevic-devtech <milivoj.kovacevic@devtechgroup.com>
2015-02-21 20:33:14 +0100

    commented out optional parameters in order to test adding clients

--- WhmcsPopulator/Api/AddClientRequest.cs
+++ WhmcsPopulator/Api/AddClientRequest.cs
@@ -41,40 +41,40 @@ namespace WhmcsPopulator.Api
         public string Password2;
         
         // Optional parameters
-        [ApiParamName("companyname")]
-        public string CompanyName;
-        [ApiParamName("address2")]
-        public string Address2;
-        [ApiParamName("currency")]
-        public string Currency;
-        [ApiParamName("clientip")]
-        public string ClientIp;
-        [ApiParamName("language")]
-        public string Language;
-        [ApiParamName("groupid")]
-        public string GroupId;
-        [ApiParamName("securityqid")]
-        public string SecurityQuestionId;
-        [ApiParamName("securityqans")]
-        public string SecurityQuestionAnswer;
-        [ApiParamName("notes")]
-        public string Notes;
-        [ApiParamName("cctype")]
-        public string CreditCardType;
-        [ApiParamName("cardnum")]
-        public string CardNumber;
-        [ApiParamName("expdate")]
-        public string ExpDate; // In the format MMYY
-        [ApiParamName("startdate")]
-        public string StartDate;
-        [ApiParamName("issuenumber")]
-        public string IssueNumber;
-        [ApiParamName("customfields")]
-        public string CustomFields; // A base64 encoded serialized array of custom field values
-        [ApiParamName("noemail")]
-        public string NoEmail; // Pass as true to surpress the client signup welcome email sending
-        [ApiParamName("skipvalidation")]
-        public string SkipValidation; // Set true to not validate or check required fields
+		//[ApiParamName("companyname")]
+		//public string CompanyName;
+		//[ApiParamName("address2")]
+		//public string Address2;
+		//[ApiParamName("currency")]
+		//public string Currency;
+		//[ApiParamName("clientip")]
+		//public string ClientIp;
+		//[ApiParamName("language")]
+		//public string Language;
+		//[ApiParamName("groupid")]
+		//public string GroupId;
+		//[ApiParamName("securityqid")]
+		//public string SecurityQuestionId;
+		//[ApiParamName("securityqans")]
+		//public string SecurityQuestionAnswer;
+		//[ApiParamName("notes")]
+		//public string Notes;
+		//[ApiParamName("cctype")]
+		//public string CreditCardType;
+		//[ApiParamName("cardnum")]
+		//public string CardNumber;
+		//[ApiParamName("expdate")]
+		//public string ExpDate; // In the format MMYY
+		//[ApiParamName("startdate")]
+		//public string StartDate;
+		//[ApiParamName("issuenumber")]
+		//public string IssueNumber;
+		//[ApiParamName("customfields")]
+		//public string CustomFields; // A base64 encoded serialized array of custom field values
+		//[ApiParamName("noemail")]
+		//public string NoEmail; // Pass as true to surpress the client signup welcome email sending
+		//[ApiParamName("skipvalidation")]
+		//public string SkipValidation; // Set true to not validate or check required fields
 
         public AddClientRequest()
             : base()
