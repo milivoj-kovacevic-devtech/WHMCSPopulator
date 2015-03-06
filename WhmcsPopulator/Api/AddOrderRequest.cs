c3bbe82ca75caa956830227bc5e0661123ba9add
milivoj-kovacevic-devtech <milivoj.kovacevic@devtechgroup.com>
2015-03-06 10:07:31 +0100

    removed filehelpers dependency from AddOrderRequest model

--- WhmcsPopulator/Api/AddOrderRequest.cs
+++ WhmcsPopulator/Api/AddOrderRequest.cs
@@ -1,16 +1,13 @@
-﻿using FileHelpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using WhmcsPopulator.Shared;
 
 namespace WhmcsPopulator.Api
 {
-	[DelimitedRecord(",")]
 	public class AddOrderRequest : WhmcsBaseRequest
 	{
 		// Required parameters
 		[MandatoryParameter]
-		[FieldIgnored]
 		[ApiParamName("clientid")]
 		public string ClientId;
 		[MandatoryParameter]
@@ -18,43 +15,33 @@ namespace WhmcsPopulator.Api
 		public string ProductId;
 		[MandatoryParameter]
 		[ApiParamName("domain")]
-		public string DomainName; // TODO Ignore this field, assing value randomized
+		public string DomainName;
 		[MandatoryParameter]
-		[FieldIgnored]
 		[ApiParamName("billingcycle")] // onetime, monthly, quarterly, semiannually, etc...
 		public string BillingCycle = "monthly";
 		[MandatoryParameter]
-		[FieldIgnored]
 		[ApiParamName("domaintype")] // set for domain registration - register or transfer
 		public string DomainType = string.Empty;
 		[MandatoryParameter]
-		[FieldIgnored]
 		[ApiParamName("regperiod")] // 1,2,3,etc...
 		public string RegPeriod = string.Empty;
 		[MandatoryParameter]
-		[FieldIgnored]
 		[ApiParamName("eppcode")] // if domain transfer
 		public string EppCode = string.Empty;
 		[MandatoryParameter]
-		[FieldIgnored]
 		[ApiParamName("nameserver1")] // first nameserver (req for domain reg only)
 		public string Nameserver1 = string.Empty;
 		[MandatoryParameter]
-		[FieldIgnored]
 		[ApiParamName("paymentmethod")] // paypal, authorize, etc...
 		public string PaymentMethod = "paypal";
 
 		// Optional parameters
-		[FieldIgnored]
 		[ApiParamName("noinvoiceemail")]
 		public string NoInvoiceEmail = "true";
-		[FieldIgnored]
 		[ApiParamName("noemail")]
 		public string NoEmail = "true";
 
-		[FieldIgnored]
 		private List<string> Domains = new List<string>() { "bulkcloudmigrations.com", "cloudmigrationservice.net", "cloudteleporter.com", "fullyautomatedservices.com" };
-		[FieldIgnored]
 		private List<string> Products = new List<string>() { "430", "433", "434", "435" };
 
 		public AddOrderRequest(string clientId)
